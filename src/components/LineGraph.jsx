1e20f459c7d74235e8054e04d3e0b9aad4cf5da8
afaqalii <afaqali4244@gmai.com>
2023-03-08 02:29:09 -0800

    Build the UI and done with little bit of styling now working on dynamic data rendering

--- src/components/LineGraph.jsx
+++ src/components/LineGraph.jsx
@@ -1,48 +1,115 @@
-import React, {useEffect, useState} from 'react'
-import { Line } from 'react-chartjs-2';
-// import { Line } from 'react-chartjs-2'
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import numeral from "numeral";
 
-const LineGraph = () => {
-  
-    const [Data, setData] = useState({})
+const options = {
+  legend: {
+    display: false,
+  },
+  elements: {
+    point: {
+      radius: 0,
+    },
+  },
+  maintainAspectRatio: false,
+  tooltips: {
+    mode: "index",
+    intersect: false,
+    callbacks: {
+      label: function (tooltipItem, data) {
+        return numeral(tooltipItem.value).format("+0,0");
+      },
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        type: "time",
+        time: {
+          format: "MM/DD/YY",
+          tooltipFormat: "ll",
+        },
+      },
+    ],
+    yAxes: [
+      {
+        gridLines: {
+          display: false,
+        },
+        ticks: {
+          // Include a dollar sign in the ticks
+          callback: function (value, index, values) {
+            return numeral(value).format("0a");
+          },
+        },
+      },
+    ],
+  },
+};
 
-
-    const buildChartData = (data, casesType='cases') => {
-        const charData = [];
-        let lastDataPoint;
-        data.casesType.forEach(data => {
-            if(lastDataPoint) {
-                const newDataPoint = {
-                    x: date,
-                    y: data[casesType][date] - lastDataPoint
-                }
-                charData.push(newDataPoint)
-            }
-            lastDataPoint = data[casesType][date];
-        })
-        return charData
+const buildChartData = (data, casesType) => {
+  let chartData = [];
+  let lastDataPoint;
+  for (let date in data.cases) {
+    if (lastDataPoint) {
+      let newDataPoint = {
+        x: date,
+        y: data[casesType][date] - lastDataPoint,
+      };
+      chartData.push(newDataPoint);
     }
+    lastDataPoint = data[casesType][date];
+  }
+  return chartData;
+};
 
+function LineGraph({ casesType}) {
+  const [data, setData] = useState({});
 
-    useEffect(() => {
-        fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
-        .then(response => response.json())
-        .then(data => {
-            const chartData = buildChartData(data, casesType)
-            setData(chartData)
-        } )
-    },[])
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          let chartData = buildChartData(data, casesType);
+          setData(chartData);
+          console.log(chartData);
+          // buildChart(chartData);
+        });
+    };
 
-    return (
+    fetchData();
+  }, [casesType]);
+
+  return (
     <div>
-        <h1>line Graph</h1>
-        {/* <Line data={{
-            datasets: [{
-                Data
-            }]
-        }}/> */}
+      {data?.length > 0 && (
+        <Line
+          data={{
+            datasets: [
+              {
+                backgroundColor: "rgba(204, 16, 52, 0.5)",
+                borderColor: "#CC1034",
+                data: data,
+              },
+            ],
+          }}
+          options={options}
+        />
+      )}
     </div>
-  )
+  );
 }
 
-export default LineGraph
+export default LineGraph;
+
+
+
+
+
+
+// MY CODE
+
+
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import {Leaflet, TileLayer } from "react-leaflet";
+// import {Leaflet, TileLayer } from "react-leaflet";
 const Map = ({center, zoom}) => {
   return (
     <div className='map'>
-      <Leaflet center={center} zoom={zoom} >
+      {/* <Leaflet center={center} zoom={zoom} >
         <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-      </Leaflet>
+      </Leaflet> */}
     </div>
   )
 }
